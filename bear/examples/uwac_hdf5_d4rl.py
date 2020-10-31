9a32fce34fe114ee8a2fd1d3e7c77bd15ab503f4
scey26 <scey26@naver.com>
2020-10-31 18:28:32 +0900

    123

--- bear/examples/uwac_hdf5_d4rl.py
+++ bear/examples/uwac_hdf5_d4rl.py
@@ -19,6 +19,9 @@ import h5py, argparse
 import gym
 import d4rl
 
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1"  # specify which GPU(s) to be used
 
 def load_hdf5(dataset, replay_buffer, max_size):
     all_obs = dataset['observations']
@@ -134,7 +137,7 @@ def experiment(variant):
 if __name__ == "__main__":
     # noinspection PyTypeChecker
     parser = argparse.ArgumentParser(description='BEAR-runs')
-    parser.add_argument("--env", type=str, default='halfcheetah-expert-v0')
+    parser.add_argument("--env", type=str, default='pen-cloned-v0')
     parser.add_argument("--gpu", default='0', type=str)
     parser.add_argument('--qf_lr', default=3e-4, type=float)
     parser.add_argument('--policy_lr', default=1e-4, type=float)
